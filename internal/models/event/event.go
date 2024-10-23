6af7e68503645be54f3545c1f5d8d533ef8748c2
assbanlucky@mail.ru <assbanlucky@mail.ru>
2024-10-23 18:58:37 +0400

    change package name

---
+++ internal/models/event/event.go
@@ -0,0 +1,30 @@
+package event
+
+import (
+	"main/internal/models/subject"
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type EventType string
+
+const (
+	RegionalStage EventType = "REGIONAL_STAGE"
+	Olympiad      EventType = "OLYMPIAD"
+	Stage         EventType = "STAGE"
+	Appeal        EventType = "APPEAL"
+)
+
+type Event struct {
+	gorm.Model
+	Name            string          `gorm:"type:varchar(128);not null"`
+	StartDate       time.Time       `gorm:"not null"`
+	EndDate         time.Time       `gorm:"not null"`
+	EventType       EventType       `gorm:"type:event_type;not null"`
+	PreviousEventID *uint           `gorm:"index"`
+	PreviousEvent   *Event          `gorm:"foreignKey:PreviousEventID"`
+	SubjectID       uint            `gorm:"index"`
+	Subject         subject.Subject `gorm:"foreignKey:SubjectID"`
+	AdditionalInfo  string          `gorm:"type:text"`
+}
