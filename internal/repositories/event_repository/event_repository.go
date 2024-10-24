bf1464986ce9a9602bd814477db4f691fe0bc89e
assbanlucky@mail.ru <assbanlucky@mail.ru>
2024-10-25 01:01:37 +0400

    added error return

--- internal/repositories/event_repository/event_repository.go
+++ internal/repositories/event_repository/event_repository.go
@@ -1,6 +1,7 @@
 package eventrepository
 
 import (
+	"fmt"
 	"main/internal/dto/event_dto"
 	"main/internal/models/event"
 
@@ -11,33 +12,37 @@ type EventRepository struct{}
 
 // Get event by ID
 func (r *EventRepository) GetEventByID(db *gorm.DB, id uint) (event.Event, error) {
+	const op = "repositories.event_repository.GetEventById"
 	event_res := event.Event{Model: gorm.Model{ID: id}}
 	if err := db.First(&event_res).Error; err != nil {
-		return event.Event{}, err
+		return event.Event{}, fmt.Errorf("%s: %w", op, err)
 	}
 	return event_res, nil
 }
 
 // Get slice events by EventType
 func (r *EventRepository) GetEventsByType(db *gorm.DB, eventType event.EventType) ([]event.Event, error) {
+	const op = "repositories.event_repository.GetEventsByType"
 	events_res := []event.Event{}
 	if err := db.Find(&events_res, event.Event{EventType: eventType}).Error; err != nil {
-		return nil, err
+		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 	return events_res, nil
 }
 
 // Get all events
 func (r *EventRepository) GetAllEvents(db *gorm.DB) ([]event.Event, error) {
+	const op = "repositories.event_repository.GetAllEvents"
 	events_res := []event.Event{}
-	if err := db.Find(events_res).Error; err != nil {
-		return nil, err
+	if err := db.Find(&events_res).Error; err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 	return events_res, nil
 }
 
 // Add new event in DB
 func (r *EventRepository) CreateEvent(db *gorm.DB, event_dto event_dto.EventDTO) (uint, error) {
+	const op = "repositories.event_repository.CreateEvent"
 	new_event := event.Event{
 		Name:            event_dto.Name,
 		StartDate:       event_dto.StartDate,
@@ -48,13 +53,14 @@ func (r *EventRepository) CreateEvent(db *gorm.DB, event_dto event_dto.EventDTO)
 		EventType:       event_dto.EventType,
 	}
 	if err := db.Create(&new_event).Error; err != nil {
-		return 0, err
+		return 0, fmt.Errorf("%s: %w", op, err)
 	}
 	return new_event.ID, nil
 }
 
 // Update event
 func (r *EventRepository) UpdateEvent(db *gorm.DB, event_dto event_dto.EventDTO) (uint, error) {
+	const op = "repositories.event_repository.UpdateEvent"
 	update_event := event.Event{
 		Model:           gorm.Model{ID: uint(event_dto.ID)},
 		Name:            event_dto.Name,
@@ -66,15 +72,16 @@ func (r *EventRepository) UpdateEvent(db *gorm.DB, event_dto event_dto.EventDTO)
 		EventType:       event_dto.EventType,
 	}
 	if err := db.Updates(&update_event).Error; err != nil {
-		return 0, err
+		return 0, fmt.Errorf("%s: %w", op, err)
 	}
 	return update_event.ID, nil
 }
 
 // Delete event
 func (r *EventRepository) DeleteEvent(db *gorm.DB, id uint) error {
+	const op = "repositories.event_repository.DeleteEvent"
 	if err := db.Delete(event.Event{}, id).Error; err != nil {
-		return err
+		return fmt.Errorf("%s: %w", op, err)
 	}
 	return nil
 }
--- internal/repositories/subject_repository/subject_repository.go
+++ internal/repositories/subject_repository/subject_repository.go
@@ -1,6 +1,7 @@
 package subjectrepository
 
 import (
+	"fmt"
 	"main/internal/dto/subject_dto"
 	"main/internal/models/subject"
 
@@ -11,47 +12,52 @@ type SubjectRepository struct{}
 
 // Get subject by id
 func (r *SubjectRepository) GetSubjectById(db *gorm.DB, id uint) (subject.Subject, error) {
+	const op = "repositories.subject_repository.GetSubjectById"
 	subject_res := subject.Subject{ID: id}
 	if err := db.First(&subject_res).Error; err != nil {
-		return subject.Subject{}, err
+		return subject.Subject{}, fmt.Errorf("%s: %w", op, err)
 	}
 	return subject_res, nil
 }
 
 // Get all subjects
 func (r *SubjectRepository) GetAllSubjects(db *gorm.DB) ([]subject.Subject, error) {
+	const op = "repositories.subject_repository.GetAllSubjects"
 	subjects_res := []subject.Subject{}
 	if err := db.Find(&subjects_res).Error; err != nil {
-		return nil, err
+		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 	return subjects_res, nil
 }
 
 // Add new subject in DB
 func (r *SubjectRepository) CreateSubject(db *gorm.DB, subject_dto subject_dto.SubjectDTO) (uint, error) {
+	const op = "repositories.subject_repository.CreateSubject"
 	new_subject := subject.Subject{Name: subject_dto.Name}
 	if err := db.Create(&new_subject).Error; err != nil {
-		return 0, err
+		return 0, fmt.Errorf("%s: %w", op, err)
 	}
 	return new_subject.ID, nil
 }
 
 // Update subject
 func (r *SubjectRepository) UpdateSubject(db *gorm.DB, subject_dto subject_dto.SubjectDTO) (uint, error) {
+	const op = "repositories.subject_repository.UpdateSubject"
 	update_subject := subject.Subject{
 		ID:   subject_dto.ID,
 		Name: subject_dto.Name,
 	}
 	if err := db.Updates(&update_subject).Error; err != nil {
-		return 0, err
+		return 0, fmt.Errorf("%s: %w", op, err)
 	}
 	return update_subject.ID, nil
 }
 
 // Delete subject
 func (r *SubjectRepository) DeleteSubject(db *gorm.DB, id uint) error {
+	const op = "repositories.subject_repository.DeleteSubject"
 	if err := db.Delete(subject.Subject{}, id).Error; err != nil {
-		return err
+		return fmt.Errorf("%s: %w", op, err)
 	}
 	return nil
 }
