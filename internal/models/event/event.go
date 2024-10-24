5fbc0ebe7b7adeffa64591417a9efd17d6983137
assbanlucky@mail.ru <assbanlucky@mail.ru>
2024-10-24 23:34:39 +0400

    field can be null

--- internal/models/event/event.go
+++ internal/models/event/event.go
@@ -18,13 +18,13 @@ const (
 
 type Event struct {
 	gorm.Model
-	Name            string          `gorm:"type:varchar(128);not null"`
-	StartDate       time.Time       `gorm:"not null"`
-	EndDate         time.Time       `gorm:"not null"`
-	EventType       EventType       `gorm:"type:event_type;not null"`
-	PreviousEventID *uint           `gorm:"index"`
-	PreviousEvent   *Event          `gorm:"foreignKey:PreviousEventID"`
-	SubjectID       uint            `gorm:"index"`
-	Subject         subject.Subject `gorm:"foreignKey:SubjectID"`
-	AdditionalInfo  string          `gorm:"type:text"`
+	Name            string           `gorm:"type:varchar(128);not null"`
+	StartDate       time.Time        `gorm:"not null"`
+	EndDate         time.Time        `gorm:"not null"`
+	EventType       EventType        `gorm:"type:event_type;not null"`
+	PreviousEventID *uint            `gorm:"index"`
+	PreviousEvent   *Event           `gorm:"foreignKey:PreviousEventID"`
+	SubjectID       *uint            `gorm:"index"`
+	Subject         *subject.Subject `gorm:"foreignKey:SubjectID"`
+	AdditionalInfo  string           `gorm:"type:text"`
 }
