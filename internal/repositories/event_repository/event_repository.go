67d802d8bc555a61c9727b87f2e1876bebf6b29a
assbanlucky@mail.ru <assbanlucky@mail.ru>
2024-10-24 23:35:24 +0400

    Added repositories

--- internal/repositories/event_repository/event_repository.go
+++ internal/repositories/event_repository/event_repository.go
@@ -1,47 +1,80 @@
 package eventrepository
 
 import (
+	"main/internal/dto/event_dto"
+	"main/internal/models/event"
+
 	"gorm.io/gorm"
 )
 
-type eventRepositoryInterfaca interface {
-	GetEventByID()
-	GetEventByType()
-	GetAllEvents()
+type EventRepository struct{}
 
-	CreateEvent()
-	UpdateEvent()
-	DeleteEvent()
+// Get event by ID
+func (r *EventRepository) GetEventByID(db *gorm.DB, id uint) (event.Event, error) {
+	event_res := event.Event{Model: gorm.Model{ID: id}}
+	if err := db.First(&event_res).Error; err != nil {
+		return event.Event{}, err
+	}
+	return event_res, nil
 }
 
-type EventRepository struct {
-	db *gorm.DB
+// Get slice events by EventType
+func (r *EventRepository) GetEventsByType(db *gorm.DB, eventType event.EventType) ([]event.Event, error) {
+	events_res := []event.Event{}
+	if err := db.Find(&events_res, event.Event{EventType: eventType}).Error; err != nil {
+		return nil, err
+	}
+	return events_res, nil
 }
 
-func NewEventRepository(db *gorm.DB) eventRepositoryInterfaca {
-	return &EventRepository{db: db}
+// Get all events
+func (r *EventRepository) GetAllEvents(db *gorm.DB) ([]event.Event, error) {
+	events_res := []event.Event{}
+	if err := db.Find(events_res).Error; err != nil {
+		return nil, err
+	}
+	return events_res, nil
 }
 
-func (r *EventRepository) GetEventByID() {
-
+// Add new event in DB
+func (r *EventRepository) CreateEvent(db *gorm.DB, event_dto event_dto.EventDTO) (uint, error) {
+	new_event := event.Event{
+		Name:            event_dto.Name,
+		StartDate:       event_dto.StartDate,
+		EndDate:         event_dto.EndDate,
+		PreviousEventID: &event_dto.PreviousEventID,
+		SubjectID:       &event_dto.SubjectID,
+		AdditionalInfo:  event_dto.AdditionalInfo,
+		EventType:       event_dto.EventType,
+	}
+	if err := db.Create(&new_event).Error; err != nil {
+		return 0, err
+	}
+	return new_event.ID, nil
 }
 
-func (r *EventRepository) GetEventByType() {
-
+// Update event
+func (r *EventRepository) UpdateEvent(db *gorm.DB, event_dto event_dto.EventDTO) (uint, error) {
+	update_event := event.Event{
+		Model:           gorm.Model{ID: uint(event_dto.ID)},
+		Name:            event_dto.Name,
+		StartDate:       event_dto.StartDate,
+		EndDate:         event_dto.EndDate,
+		PreviousEventID: &event_dto.PreviousEventID,
+		SubjectID:       &event_dto.SubjectID,
+		AdditionalInfo:  event_dto.AdditionalInfo,
+		EventType:       event_dto.EventType,
+	}
+	if err := db.Updates(&update_event).Error; err != nil {
+		return 0, err
+	}
+	return update_event.ID, nil
 }
 
-func (r *EventRepository) GetAllEvents() {
-
-}
-
-func (r *EventRepository) CreateEvent() {
-
-}
-
-func (r *EventRepository) UpdateEvent() {
-
-}
-
-func (r *EventRepository) DeleteEvent() {
-
+// Delete event
+func (r *EventRepository) DeleteEvent(db *gorm.DB, id uint) error {
+	if err := db.Delete(event.Event{}, id).Error; err != nil {
+		return err
+	}
+	return nil
 }
--- internal/repositories/subject_repository/subject_repository.go
+++ internal/repositories/subject_repository/subject_repository.go
@@ -1,5 +1,57 @@
 package subjectrepository
 
-// TODO interface
+import (
+	"main/internal/dto/subject_dto"
+	"main/internal/models/subject"
 
-// TODO SubjectRepositiry
+	"gorm.io/gorm"
+)
+
+type SubjectRepository struct{}
+
+// Get subject by id
+func (r *SubjectRepository) GetSubjectById(db *gorm.DB, id uint) (subject.Subject, error) {
+	subject_res := subject.Subject{ID: id}
+	if err := db.First(&subject_res).Error; err != nil {
+		return subject.Subject{}, err
+	}
+	return subject_res, nil
+}
+
+// Get all subjects
+func (r *SubjectRepository) GetAllSubjects(db *gorm.DB) ([]subject.Subject, error) {
+	subjects_res := []subject.Subject{}
+	if err := db.Find(&subjects_res).Error; err != nil {
+		return nil, err
+	}
+	return subjects_res, nil
+}
+
+// Add new subject in DB
+func (r *SubjectRepository) CreateSubject(db *gorm.DB, subject_dto subject_dto.SubjectDTO) (uint, error) {
+	new_subject := subject.Subject{Name: subject_dto.Name}
+	if err := db.Create(&new_subject).Error; err != nil {
+		return 0, err
+	}
+	return new_subject.ID, nil
+}
+
+// Update subject
+func (r *SubjectRepository) UpdateSubject(db *gorm.DB, subject_dto subject_dto.SubjectDTO) (uint, error) {
+	update_subject := subject.Subject{
+		ID:   subject_dto.ID,
+		Name: subject_dto.Name,
+	}
+	if err := db.Updates(&update_subject).Error; err != nil {
+		return 0, err
+	}
+	return update_subject.ID, nil
+}
+
+// Delete subject
+func (r *SubjectRepository) DeleteSubject(db *gorm.DB, id uint) error {
+	if err := db.Delete(subject.Subject{}, id).Error; err != nil {
+		return err
+	}
+	return nil
+}
