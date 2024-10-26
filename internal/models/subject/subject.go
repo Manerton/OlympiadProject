a41915f956bae085e574a8e5de3b0afd569d5c7f
assbanlucky@mail.ru <assbanlucky@mail.ru>
2024-10-26 22:38:19 +0400

    Name unique

--- internal/models/subject/subject.go
+++ internal/models/subject/subject.go
@@ -2,5 +2,5 @@ package subject
 
 type Subject struct {
 	ID   uint   `gorm:"primaryKey"`
-	Name string `gorm:"type:varchar(100);not null"`
+	Name string `gorm:"type:varchar(100);not null;unique"`
 }
