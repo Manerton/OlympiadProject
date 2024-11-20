6ea9438914628a683591a06c8981ced5207d574a
Manerton <assbanlucky@gmail.com>
2024-11-21 02:43:23 +0400

    add support cors/new endpoints/gorutine start server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"log/slog"
 	"main/internal/config"
 	"main/internal/handlers/event_handler"
@@ -12,9 +13,14 @@ import (
 	"main/internal/services/event_service"
 	"main/internal/storage/postgresql"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 const LocalFilePath = "config-yaml/local.yaml"
@@ -42,6 +48,16 @@ func main() {
 	// init middlewares
 	router.Use(midlogger.New(log))
 	router.Use(middleware.URLFormat)
+	// init cors
+	corsOptions := cors.Options{
+		AllowedOrigins:   []string{cfg.ReactVision}, // React URL
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300, // В секундах
+	}
+	router.Use(cors.Handler(corsOptions))
 	// init subject service and handler
 	// subjectService := subject_service.NewSubjectService(storage, &subject_repository.SubjectRepository{})
 	subjectStorage := subject.NewSubjectsStorage()
@@ -58,23 +74,38 @@ func main() {
 	// router.Delete("/subjects/{id}", subjectHandler.DeleteSubject)
 
 	// init events route
+	router.Get("/events/details/one", eventHandler.GetEventByFilterAndFields)
+	router.Get("/events/details", eventHandler.GetEventsByFilterAndFields)
 	router.Get("/events", eventHandler.GetAllEvents)
 	router.Get("/events/{id}", eventHandler.GetEventByID)
-	router.Get("/events/regional-stage", eventHandler.GetAllEventsTypeRegionalStage)
+	router.Get("/events/regional-stage", eventHandler.GetEventsTypeRegionalStage)
 	router.Get("/events/child/{id}", eventHandler.GetEventsByPreviousID)
+	router.Get("/events/list/", eventHandler.GetEventsByListID)
 	router.Post("/events", eventHandler.CreateEvent)
 	router.Put("/events/{id}", eventHandler.UpdateEvent)
 	router.Delete("/events/{id}", eventHandler.DeleteEvent)
-
 	// init server
 	server := &http.Server{
 		Addr:    cfg.GetAddress(),
 		Handler: router,
 	}
-	// start server
-	if err := server.ListenAndServe(); err != nil {
-		log.Error("failed to start server")
-	}
+	// init gorutine for start server
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			log.Error("failed to start server")
+		}
+	}()
+	log.Error("server start")
+	<-done
 
-	log.Error("server stopped")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Error("failed to stop server", liblogger.Err(err))
+		return
+	}
+	log.Info("server stopped")
 }
