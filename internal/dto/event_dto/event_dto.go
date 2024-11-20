a7701018b704b3ae77d82038ac5f8e07e94dc671
Manerton <assbanlucky@gmail.com>
2024-11-21 02:47:10 +0400

    add validate / new struct for detaild request/response

--- internal/dto/event_dto/event_dto.go
+++ internal/dto/event_dto/event_dto.go
@@ -7,11 +7,22 @@ import (
 
 type EventDTO struct {
 	ID              uint
-	Name            string
-	StartDate       time.Time
-	EndDate         time.Time
+	Name            string    `validate:"required"`
+	StartDate       time.Time `validate:"required"`
+	EndDate         time.Time `validate:"required"`
 	EventType       event.EventType
 	PreviousEventID *uint
 	Subject         string
 	AdditionalInfo  string
+}
+
+type DitailsEvent struct {
+	ID              uint            `json:"id,omitempty"`
+	Name            string          `json:"name,omitempty"`
+	StartDate       *time.Time      `json:"start_date,omitempty"` // указатель для учета нулевых значений
+	EndDate         *time.Time      `json:"end_date,omitempty"`   // указатель для учета нулевых значений
+	EventType       event.EventType `json:"event_type,omitempty"`
+	PreviousEventID *uint           `json:"previous_event_id,omitempty"`
+	Subject         string          `json:"subject,omitempty"`
+	AdditionalInfo  string          `json:"additional_info,omitempty"`
 }
