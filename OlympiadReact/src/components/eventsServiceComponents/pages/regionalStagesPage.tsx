1052b38da19bb1608f8fd220d6947e6b81785897
Manerton <assbanlucky@gmail.com>
2024-11-29 01:40:08 +0400

    use role

--- OlympiadReact/src/components/eventsServiceComponents/pages/regionalStagesPage.tsx
+++ OlympiadReact/src/components/eventsServiceComponents/pages/regionalStagesPage.tsx
@@ -1,9 +1,14 @@
 import { REGIONAL_STAGE } from "../../../types/event"
+import { RoleProvider } from "../../RoleContext"
 import BaseEventPage from "./baseEventPage"
 
 function RegionalStagesPage() {
     return (
+    <RoleProvider>
+
         <BaseEventPage type={REGIONAL_STAGE}  pageName="Региональные этапы" />
+    </RoleProvider>
+
     )
 }
 
