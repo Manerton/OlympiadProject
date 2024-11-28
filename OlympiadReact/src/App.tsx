fce5935e13960f65b5d26cd0d5d7626eacd2bd48
Manerton <assbanlucky@gmail.com>
2024-11-28 16:25:02 +0400

    add event in header

--- OlympiadReact/src/App.tsx
+++ OlympiadReact/src/App.tsx
@@ -5,22 +5,31 @@ import AttendancePage from './components/AttendaceList'
 import './App.css'
 import ProfilePage from './components/Profile'
 import Login from './components/Login'
+import SubStagePage from './components/eventsServiceComponents/pages/subStagePage'
+import OlympiadStagesPage from './components/eventsServiceComponents/pages/olympiadStagesPage'
+import RegionalStagesPage from './components/eventsServiceComponents/pages/regionalStagesPage'
+import OlympiadsPage from './components/eventsServiceComponents/pages/olympiadsPage'
 
 function App() {
- 
+
 
   return (
     <Router>
-    <Header />
-    <div className="container mt-4">
-    <Routes>
-        <Route path="/applications"  element={<ApplicationsPage />} />
-        <Route path="/attendance"  element={<AttendancePage />} />
-        <Route path="/profile"  element={<ProfilePage />} />
-        <Route path="/login"  element={<Login />} />
-    </Routes>
-    </div>
-  </Router>
+      <Header />
+      <div className="container mt-4">
+        <Routes>
+          <Route path="/events" element={<RegionalStagesPage />} />
+          <Route path="/olympiads/:id" element={<OlympiadsPage />} />
+          <Route path="/olympiad-stages/:id" element={<OlympiadStagesPage />} />
+          <Route path="/sub-stage/:id" element={<SubStagePage />}/>
+          <Route path="/applications" element={<ApplicationsPage />} />
+
+          <Route path="/attendance" element={<AttendancePage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
