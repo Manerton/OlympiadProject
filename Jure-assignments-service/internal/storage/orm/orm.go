a75448e7222c4d4681bc07aa18cc2730ca71825d
Manerton <assbanlucky@gmail.com>
2024-12-09 19:11:04 +0400

    add orm interface

--- Jure-assignments-service/internal/storage/orm/orm.go
+++ Jure-assignments-service/internal/storage/orm/orm.go
@@ -1,39 +1,72 @@
 package orm
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+
+	"gorm.io/gorm"
+)
 
 type ORM interface {
-	Create(interface{})
-	Updates(interface{})
-	Delete(interface{})
-	Find(interface{})
-	First(interface{})
+	Create(interface{}) error
+	Updates(interface{}) error
+	Delete(interface{}, ...interface{}) error
+	Find(interface{}, ...interface{}) error
+	FindWithSelect([]string, interface{}, ...interface{}) error
+	First(interface{}, ...interface{}) error
 }
 
 type Gorm struct {
-	db *gorm.DB
+	DB *gorm.DB
 }
 
-func NewGorm(db *gorm.DB) ORM {
-	return &Gorm{db: db}
+func NewGormORM(db *gorm.DB) ORM {
+	return &Gorm{DB: db}
 }
 
-func (g *Gorm) Create(interFace interface{}) {
-	g.db.Create(interFace)
+func (g *Gorm) Create(dest interface{}) error {
+	const op = "storage.orm.Create"
+	if err := g.DB.Create(dest).Error; err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+	return nil
 }
 
-func (g *Gorm) Updates(interFace interface{}) {
-	g.db.Updates(interFace)
+func (g *Gorm) Updates(dest interface{}) error {
+	const op = "storage.orm.Update"
+	if err := g.DB.Updates(dest).Error; err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+	return nil
 }
 
-func (g *Gorm) Delete(interFace interface{}) {
-	g.db.Delete(interFace)
+func (g *Gorm) Delete(dest interface{}, conds ...interface{}) error {
+	const op = "storage.orm.Delete"
+	if err := g.DB.Delete(dest, conds...).Error; err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+	return nil
 }
 
-func (g *Gorm) Find(interFace interface{}) {
-	g.db.Find(interFace)
+func (g *Gorm) Find(dest interface{}, conds ...interface{}) error {
+	const op = "storage.orm.Find"
+	if err := g.DB.Find(dest, conds...).Error; err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+	return nil
 }
 
-func (g *Gorm) First(interFace interface{}) {
-	g.db.First(interFace)
+func (g *Gorm) FindWithSelect(fields []string, dest interface{}, conds ...interface{}) error {
+	const op = "storage.orm.Find"
+	if err := g.DB.Select(fields).Find(dest, conds...).Error; err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+	return nil
+}
+
+func (g *Gorm) First(dest interface{}, conds ...interface{}) error {
+	const op = "storage.orm.First"
+	if err := g.DB.First(dest, conds...).Error; err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+	return nil
 }
