c45a9dffaffa86069080dfd32956c880841fc48c
Manerton <assbanlucky@gmail.com>
2024-12-11 18:00:12 +0400

    add api config

---
+++ OlympiadReact/src/config/apiConfig.ts
@@ -0,0 +1,7 @@
+const API_CONFIG = {
+    EVENTS: "http://localhost:8080/events",
+    JUREASSIGNMENTS: "http://localhost:8090/jury-assignments",
+    APPLICATION: "http://localhost:8080/applications",
+};
+
+export default API_CONFIG;
