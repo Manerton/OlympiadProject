50f99deb39297283119e86603795f12b3f6265a8
Manerton <assbanlucky@gmail.com>
2024-12-11 11:05:46 +0400

    add method transaction

--- Jure-assignments-service/internal/storage/orm/orm.go
+++ Jure-assignments-service/internal/storage/orm/orm.go
@@ -13,6 +13,9 @@ type ORM interface {
 	Find(interface{}, ...interface{}) error
 	FindWithSelect([]string, interface{}, ...interface{}) error
 	First(interface{}, ...interface{}) error
+	TransactionBegin() (ORM, error)
+	TransactionCommit() error
+	TransactionRollback() error
 }
 
 type Gorm struct {
@@ -67,6 +70,28 @@ func (g *Gorm) First(dest interface{}, conds ...interface{}) error {
 	const op = "storage.orm.First"
 	if err := g.DB.First(dest, conds...).Error; err != nil {
 		return fmt.Errorf("%s: %w", op, err)
+	}
+	return nil
+}
+
+func (g *Gorm) TransactionBegin() (ORM, error) {
+	tx := g.DB.Begin()
+	if tx.Error != nil {
+		return nil, tx.Error
+	}
+	return &Gorm{DB: tx}, nil
+}
+
+func (g *Gorm) TransactionCommit() error {
+	if err := g.DB.Commit().Error; err != nil {
+		return fmt.Errorf("transaction commit failed: %w", err)
+	}
+	return nil
+}
+
+func (g *Gorm) TransactionRollback() error {
+	if err := g.DB.Rollback().Error; err != nil {
+		return fmt.Errorf("transaction rollback failed: %w", err)
 	}
 	return nil
 }
