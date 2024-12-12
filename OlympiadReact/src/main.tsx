5c230dfd29db62f409121baa0022d50958b57ea2
Manerton <assbanlucky@gmail.com>
2024-12-12 19:33:59 +0400

    add support dropdown menu

--- OlympiadReact/src/main.tsx
+++ OlympiadReact/src/main.tsx
@@ -4,6 +4,8 @@ import { createRoot } from 'react-dom/client'
 import React from 'react'
 import App from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 createRoot(document.getElementById('root')!).render(
   <App />
