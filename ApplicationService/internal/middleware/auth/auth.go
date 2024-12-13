6c4ce3d3f15513f025486a69be94b5603999338c
EscBar <timuryyaga@gmail.com>
2024-12-13 13:29:26 +0400

    Removed old auth from Application

--- ApplicationService/internal/middleware/auth/auth.go
+++ ApplicationService/internal/middleware/auth/auth.go
@@ -1,14 +1,24 @@
 package auth
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"reflect"
 
 	"github.com/golang-jwt/jwt/v5"
 )
 
+type UserInfoKey struct{}
+
+type UserInfo struct {
+	id   float64
+	role string
+}
+
 // Function to verify JWT tokens
-func verifyToken(tokenString string, secretKey string) (*jwt.Token, error) {
+func verifyToken(tokenString string, secretKey []byte) (*jwt.Token, error) {
 
 	// Parse the token with the secret key
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
@@ -17,7 +27,7 @@ func verifyToken(tokenString string, secretKey string) (*jwt.Token, error) {
 
 	// Check for verification errors
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("FAILED!: %w", err)
 	}
 
 	// Check if the token is valid
@@ -29,28 +39,74 @@ func verifyToken(tokenString string, secretKey string) (*jwt.Token, error) {
 	return token, nil
 }
 
-func authenticateMiddleware(next http.Handler, key string) http.Handler {
+func AuthenticateMiddleware(next http.Handler, key string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
 		// Retrieve the token from the cookie
 		cookie, err := r.Cookie("token")
 		if err != nil {
-			fmt.Println("Token missing in cookie")
-			http.Redirect(w, r, "/login", http.StatusSeeOther)
+			log.Println("Token missing in cookie")
 			return
 		}
 
 		// Verify the token
-		token, err := verifyToken(cookie.Value, key)
+		token, err := verifyToken(cookie.Value, []byte(key))
 		if err != nil {
-			fmt.Printf("Token verification failed: %v\n", err)
-			http.Redirect(w, r, "/login", http.StatusSeeOther)
+			log.Printf("Token verification failed: %v\n", err)
 			return
 		}
 
 		// Print information about the verified token
 		fmt.Printf("Token verified successfully. Claims: %+v\n", token.Claims)
 
+		claims, ok := token.Claims.(jwt.MapClaims)
+		if !ok {
+			log.Println("Invalid token claims")
+			http.Error(w, "Invalid token claims", http.StatusUnauthorized)
+			return
+		}
+
+		fmt.Println(claims["role"], reflect.TypeOf(claims["role"]))
+		// Get role
+		role, ok := claims["role"].(string)
+		if !ok {
+			log.Println("Role missing in JWT")
+			http.Error(w, "Role missing in JWT", http.StatusForbidden)
+			return
+		}
+
+		fmt.Println(claims["id"], reflect.TypeOf(claims["id"]))
+		// Get id
+		id, ok := claims["id"].(float64)
+		if !ok {
+			log.Println("Id missing in JWT")
+			http.Error(w, "Id missing in JWT", http.StatusForbidden)
+			return
+		}
+
+		// Init struct
+		userInfo := UserInfo{id: id, role: role}
+		// Add struct in context
+		r = r.WithContext(context.WithValue(r.Context(), UserInfoKey{}, userInfo))
+
 		// Continue with the next middleware or route handler
 		next.ServeHTTP(w, r)
 	})
+}
+
+// Check role for access
+func RoleBasedAccess(requiredRole string) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			log.Println("Start rolebaseAccess")
+			user := r.Context().Value(UserInfoKey{}).(UserInfo)
+
+			if user.role != requiredRole {
+				http.Error(w, "Access denied", http.StatusForbidden)
+				return
+			}
+
+			next.ServeHTTP(w, r)
+		})
+	}
 }
