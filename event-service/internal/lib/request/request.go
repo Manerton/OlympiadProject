4fdbaf78354231f8dbb2299731ea253e41687b36
Manerton <assbanlucky@gmail.com>
2024-12-18 21:28:06 +0400

    add json literal

--- event-service/internal/lib/request/request.go
+++ event-service/internal/lib/request/request.go
@@ -3,8 +3,8 @@ package request
 import "main/internal/dto/event_dto"
 
 type DetailRequest struct {
-	Fields *[]string
-	Limit  *int
-	Offset *int
+	Fields *[]string `json:"fields"`
+	Limit  *int      `json:"limit"`
+	Offset *int      `json:"offset"`
 	event_dto.EventDTO
 }
