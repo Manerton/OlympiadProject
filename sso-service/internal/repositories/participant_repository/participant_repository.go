e0c3553171c6ecf5374e2fd3c76f3795437d1bcf
Manerton <assbanlucky@gmail.com>
2025-06-01 21:00:48 +0400

    add repository

---
+++ sso-service/internal/repositories/participant_repository/participant_repository.go
@@ -0,0 +1,53 @@
+package participant_repository
+
+import (
+	"context"
+	"fmt"
+	"main/internal/models/participant"
+	"main/internal/storage/orm"
+
+	"github.com/google/uuid"
+)
+
+type ParticipantRepository struct{}
+
+func (r *ParticipantRepository) GetByUserId(ctx context.Context, orm orm.ORM, userId uuid.UUID) (participant.Participant, error) {
+	const op = "repositories.participant_repository.GetByUserId"
+
+	participantRes := participant.Participant{UserId: userId}
+	err := orm.First(ctx, participant.Participant{}, nil, &participantRes)
+	if err != nil {
+		return participant.Participant{}, fmt.Errorf("%s: %w", op, err)
+	}
+	return participantRes, nil
+}
+
+func (r *ParticipantRepository) Create(ctx context.Context, orm orm.ORM, participant *participant.Participant) (uuid.UUID, error) {
+	const op = "repositories.participant_repository.Create"
+
+	err := orm.Create(ctx, participant)
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return participant.ID, nil
+}
+func (r *ParticipantRepository) Update(ctx context.Context, orm orm.ORM, participant *participant.Participant) (uuid.UUID, error) {
+	const op = "repositories.participant_repository.Update"
+
+	err := orm.Updates(ctx, participant)
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("%s: %w", op, err)
+	}
+	return participant.ID, nil
+}
+
+func (r *ParticipantRepository) Delete(ctx context.Context, orm orm.ORM, id uuid.UUID) error {
+	const op = "repositories.participant_repository.Delete"
+
+	err := orm.Delete(ctx, &participant.Participant{ID: id})
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+	return nil
+}
--- sso-service/internal/repositories/user_repository/user_repository.go
+++ sso-service/internal/repositories/user_repository/user_repository.go
@@ -9,15 +9,13 @@ import (
 	"github.com/google/uuid"
 )
 
-type UserRepository struct {
-	model user.User
-}
+type UserRepository struct{}
 
 func (r *UserRepository) GetByEmail(ctx context.Context, orm orm.ORM, email string) (user.User, error) {
 	const op = "repositories.UserRepository.GetByEmail"
 
 	userResult := user.User{}
-	err := orm.First(ctx, r.model, nil, &userResult, user.User{Email: email})
+	err := orm.First(ctx, user.User{}, nil, &userResult, user.User{Email: email})
 	if err != nil {
 		return user.User{}, fmt.Errorf("%s: %w", op, err)
 	}
@@ -35,13 +33,25 @@ func (r *UserRepository) GetByListId(ctx context.Context, orm orm.ORM, ids []uui
 	return []user.User{}, nil
 }
 
-func (r *UserRepository) Create(ctx context.Context, orm orm.ORM, user user.User) (uuid.UUID, error) {
+func (r *UserRepository) Create(ctx context.Context, orm orm.ORM, user *user.User) (uuid.UUID, error) {
 	const op = "repositories.UserRepository.Create"
 
-	err := orm.Create(ctx, &user)
+	err := orm.Create(ctx, user)
 	if err != nil {
 		return uuid.Nil, fmt.Errorf("%s: %w", op, err)
 	}
-
 	return user.ID, nil
+}
+
+func (r *UserRepository) Update(ctx context.Context, orm orm.ORM, user *user.User) error {
+	return nil
+}
+
+func (r *UserRepository) Delete(ctx context.Context, orm orm.ORM, id uuid.UUID) error {
+	const op = "repositories.user_repository.Delete"
+	err := orm.Delete(ctx, &user.User{ID: id})
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+	return nil
 }
