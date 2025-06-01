f276e1f83f20144e79d720f38923328cf0e11342
Manerton <assbanlucky@gmail.com>
2025-06-01 20:58:51 +0400

    add mapper

---
+++ sso-service/internal/lib/mapper/user_mapper/user_mapper.go
@@ -0,0 +1,33 @@
+package user_mapper
+
+import (
+	register_dto "main/internal/dto/auth/register"
+	user_dto "main/internal/dto/user"
+	"main/internal/models/user"
+)
+
+func ToDTO(user user.User) user_dto.UserResponseDTO {
+	return user_dto.UserResponseDTO{
+		ID:          user.ID.String(),
+		Email:       user.Email,
+		FirstName:   user.FirstName,
+		LastName:    user.LastName,
+		Surname:     user.Surname,
+		Sex:         user.Sex,
+		Role:        string(user.Role),
+		PhoneNumber: user.PhoneNumber,
+		BirthDate:   user.BirthDate.String(),
+	}
+}
+
+func FromRegisterToModel(regiterDTO *register_dto.RegisterParticipantRequestDTO) user.User {
+	return user.User{
+		Email:       regiterDTO.Email,
+		FirstName:   regiterDTO.FirstName,
+		LastName:    regiterDTO.LastName,
+		Surname:     regiterDTO.Surname,
+		PhoneNumber: regiterDTO.PhoneNumber,
+		BirthDate:   regiterDTO.BirthDate,
+		Sex:         regiterDTO.Sex,
+	}
+}
