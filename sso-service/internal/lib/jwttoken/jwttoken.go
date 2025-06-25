9c15e8150d3069dffe897b0e6682b1fea57a65a3
Manerton <assbanlucky@gmail.com>
2025-06-25 16:50:46 +0400

    add get claims

--- sso-service/internal/lib/jwttoken/jwttoken.go
+++ sso-service/internal/lib/jwttoken/jwttoken.go
@@ -6,7 +6,13 @@ import (
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 )
+
+type TokenClaims struct {
+	ID     uuid.UUID
+	UserId uuid.UUID
+}
 
 type JWTManager struct {
 	secretKey       []byte
@@ -28,6 +34,54 @@ func (m *JWTManager) GetAccessDuration() time.Duration {
 
 func (m *JWTManager) GetRefreshDuration() time.Duration {
 	return m.refreshDuration
+}
+
+func (m *JWTManager) GetRefreshClaims(token *jwt.Token) (*TokenClaims, error) {
+	resultClaims, err := m.GetClaims(token)
+	if err != nil {
+		return nil, err
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return nil, fmt.Errorf("failed get claims")
+	}
+
+	// exist only refresh token
+	id, ok := claims["id"].(string)
+	if !ok {
+		return nil, fmt.Errorf("failed get id on claims")
+	}
+
+	uid, err := uuid.Parse(id)
+	if err != nil {
+		return nil, fmt.Errorf("failed parse id to uuid: %w", err)
+	}
+	resultClaims.ID = uid
+
+	return resultClaims, nil
+}
+
+func (m *JWTManager) GetClaims(token *jwt.Token) (*TokenClaims, error) {
+	resultClaims := &TokenClaims{}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return nil, fmt.Errorf("failed get claims")
+	}
+
+	userId, ok := claims["user_id"].(string)
+	if !ok {
+		return nil, fmt.Errorf("failed get user_id on claims")
+	}
+
+	uid, err := uuid.Parse(userId)
+	if err != nil {
+		return nil, fmt.Errorf("failed parse user_id to uuid: %w", err)
+	}
+	resultClaims.UserId = uid
+
+	return resultClaims, nil
 }
 
 func (m *JWTManager) CreateToken(user user.User) (string, error) {
@@ -62,18 +116,18 @@ func (m *JWTManager) CreateRefreshToken(user user.User) (string, error) {
 	return tokenStr, nil
 }
 
-func (m *JWTManager) VerifyToken(token string) (bool, error) {
+func (m *JWTManager) VerifyToken(token string) (*jwt.Token, error) {
 	tokenParsed, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
 		return m.secretKey, nil
 	})
 
 	if err != nil {
-		return false, err
+		return nil, err
 	}
 
 	if !tokenParsed.Valid {
-		return false, fmt.Errorf("invalid token")
+		return nil, fmt.Errorf("invalid token")
 	}
 
-	return true, nil
+	return tokenParsed, nil
 }
