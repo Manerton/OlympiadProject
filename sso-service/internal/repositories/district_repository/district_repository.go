b146c275714836b9c215fd52dd569912033dff28
Manerton <assbanlucky@gmail.com>
2025-11-19 17:12:45 +0400

    fix get districts

--- sso-service/internal/repositories/district_repository/district_repository.go
+++ sso-service/internal/repositories/district_repository/district_repository.go
@@ -20,7 +20,7 @@ func New() *DistrictRepository {
 func (r *DistrictRepository) GetAllByRegion(ctx context.Context, orm orm.ORM, region int) ([]district.District, error) {
 	const op = "repository.DistrictRepository.GetAllByRegion"
 	result := []district.District{}
-	err := orm.First(ctx, district.District{}, nil, &result, district.District{Region: region_dictionary.RegionID(region)})
+	err := orm.Find(ctx, district.District{}, nil, nil, nil, nil, &result, district.District{Region: region_dictionary.RegionID(region)})
 	if err != nil {
 		return nil, fmt.Errorf("%s: %w", op, err)
 	}
@@ -31,7 +31,7 @@ func (r *DistrictRepository) GetAllByRegion(ctx context.Context, orm orm.ORM, re
 func (r *DistrictRepository) GetByID(ctx context.Context, orm orm.ORM, id uuid.UUID) (district.District, error) {
 	const op = "repository.DistrictRepository.GetByID"
 	result := district.District{}
-	err := orm.Find(ctx, district.District{}, nil, nil, nil, nil, &result, district.District{ID: id})
+	err := orm.First(ctx, district.District{}, nil, &result, district.District{ID: id})
 	if err != nil {
 		return district.District{}, fmt.Errorf("%s: %w", op, err)
 	}
